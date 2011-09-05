65ce195f6a012947637a9a4b304a52dbcc8b8739
pianomanDylan <pianomanDylan@b0e6e2f3-db41-2e0f-3037-ca2060a14180>
2011-09-05 19:21:16 +0000

    Got rid of the autogenerated package with an awful underscored name. Only needed to keep the Perspective, which can probably be removed later

---
+++ Image Template Changer/src/com/dylanh/itc/editor/SWTImageCanvas.java
@@ -0,0 +1,12 @@
+package com.dylanh.itc.editor;
+
+import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Composite;
+
+public class SWTImageCanvas extends Canvas {
+
+	public SWTImageCanvas(Composite parent, int style) {
+		super(parent, style);
+	}
+
+}
